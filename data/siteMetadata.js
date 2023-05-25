923a509b8795cfe168979f476a192d3af9e83768
HanhPham1512 <39138220+HanhPham1512@users.noreply.github.com>
2023-05-25 10:15:28 +1000

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -2,7 +2,7 @@ const siteMetadata = {
   title: 'Next.js Starter Blog',
   author: 'Tails Azimuth',
   headerTitle: 'Hannah Pham',
-  description: 'A blog created with Next.js and Tailwind.css',
+  description: 'A simple portfolio created with love and hard working',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
