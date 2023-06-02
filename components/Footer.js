01adee1fc3a001daff09feb981a8038c8b38ba6b
HanhPham1512 <39138220+HanhPham1512@users.noreply.github.com>
2023-06-02 12:29:49 +1000

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -23,8 +23,8 @@ export default function Footer() {
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
           <Link href="https://github.com/HanhPham1512">
-            Hannah Pham
-          </Link>
+            Hannah
+            </Link>
         </div>
       </div>
     </footer>
