a3fdb6c05aabd257248b7d8a001b3a837437e2ab
HanhPham1512 <39138220+HanhPham1512@users.noreply.github.com>
2023-06-02 14:00:01 +1000

    Update tags.js

--- pages/tags.js
+++ pages/tags.js
@@ -6,13 +6,13 @@ export default function CV() {
   return (
     <>
       <PageSEO title={`CV - ${siteMetadata.author}`} description="My Curriculum Vitae" />
-      <div className="mt-24 items-center justify-center flex flex-col">
+      <div className="mt-24 flex flex-col items-center justify-center">
         <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10">
           My Curriculum Vitae
         </h1>
         <div className="mt-8">
           <Link
-            href={siteMetadata.cvUrl}  // Replace with the URL to your CV
+            href={siteMetadata.cvUrl} // Replace with the URL to your CV
             className="rounded-md bg-blue-600 px-6 py-3 text-white hover:bg-blue-700"
           >
             Download CV
