e6d1064e66a84da1459425646a315536892f7a79
HanhPham1512 <39138220+HanhPham1512@users.noreply.github.com>
2023-06-02 13:43:56 +1000

    Update tags.js

--- pages/tags.js
+++ pages/tags.js
@@ -1,30 +1,24 @@
+import Link from '@/components/Link'
 import { PageSEO } from '@/components/SEO'
 import siteMetadata from '@/data/siteMetadata'
-import NextLink from 'next/link'
 
 export default function CV() {
   return (
     <>
       <PageSEO title={`CV - ${siteMetadata.author}`} description="My Curriculum Vitae" />
-      <div className="mt-24 flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-center mt-24">
         <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10">
           My Curriculum Vitae
         </h1>
         <div className="mt-8">
-          <NextLink href={siteMetadata.cvUrl}>
-            <a className="rounded-md bg-blue-600 px-6 py-3 text-white hover:bg-blue-700">
-              Download CV
-            </a>
-          </NextLink>
+          <Link
+            href={siteMetadata.cvUrl}  // Replace with the URL to your CV
+            className="rounded-md bg-blue-600 px-6 py-3 text-white hover:bg-blue-700"
+          >
+            Download CV
+          </Link>
         </div>
       </div>
     </>
   )
 }
-
-
-
-
-
-
-
