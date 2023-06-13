422557d2d95829e41176cc6e61ece8b35e8bfd8a
Hannah Pham <39138220+HanhPham1512@users.noreply.github.com>
2023-06-13 14:28:24 +1000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,44 +4,22 @@ import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { getAllFilesFrontMatter } from '@/lib/mdx'
 import formatDate from '@/lib/utils/formatDate'
-import React, { useState } from 'react'
+
 
 export default function Home({ posts }) {
-  const [selectedImage, setSelectedImage] = useState(null)
-
-  const handleImageUpload = (event) => {
-    const file = event.target.files[0]
-    setSelectedImage(URL.createObjectURL(file))
-  }
-
-  const checkCount = () => {
-    const countInput = document.getElementById('count-input')
-    const count = parseInt(countInput.value)
-
-    if (count === 5) {
-      document.getElementById('result').textContent = "Congratulations! You found all 'hello' words."
-    } else if (count > 5) {
-      document.getElementById('result').textContent = "Oops! There are only 5 'hello' words."
-    } else {
-      document.getElementById('result').textContent = "Not quite there yet. Keep searching!"
-    }
-  }
-
   return (
     <>
-      <div>
-        <h1>Welcome!</h1>
-        <p>{siteMetadata.description}</p>
-      </div>
-      <div>
-        <h2>Image Upload</h2>
-        <input type="file" accept="image/*" onChange={handleImageUpload} />
-        {selectedImage && <img src="/static/images/SimpleWord.png" alt="game" />}
-      </div>
-      <label htmlFor="count-input">How many 'hello' words can you find?</label>
-      <input type="number" id="count-input" />
-      <button onClick={checkCount}>Check</button>
-      <p id="result"></p>
+      <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Latest
+          </h1>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            {siteMetadata.description}
+          </p>
+        </div>
+   
     </>
   )
 }
