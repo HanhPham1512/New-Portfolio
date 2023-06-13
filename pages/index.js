b2df805a35021b21462fda998d1e3f8c81f03537
Hannah Pham <39138220+HanhPham1512@users.noreply.github.com>
2023-06-13 15:10:09 +1000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,26 @@
-import Link from '@/components/Link'
-import { PageSEO } from '@/components/SEO'
-import Tag from '@/components/Tag'
-import siteMetadata from '@/data/siteMetadata'
-import { getAllFilesFrontMatter } from '@/lib/mdx'
-import formatDate from '@/lib/utils/formatDate'
-import { useState } from 'react';
-
 const imageSrc = '/static/images/SimpleWord.png';
 
-export default function Home() {
-  const [count, setCount] = useState(0);
+function checkCount() {
+  const countInput = document.getElementById('count-input');
+  const count = parseInt(countInput.value);
+  const result = document.getElementById('result');
 
-  function handleCountChange(event) {
-    setCount(parseInt(event.target.value));
+  if (count === 5) {
+    result.textContent = "Congratulations! You found all 'hello' words.";
+  } else if (count > 5) {
+    result.textContent = "Oops! There are only 5 'hello' words.";
+  } else {
+    result.textContent = "Not quite there yet. Keep searching!";
   }
-
-  function checkCount() {
-    let result = '';
-
-    if (count === 5) {
-      result = "Congratulations! You found all 'hello' words.";
-    } else if (count > 5) {
-      result = "Oops! There are only 5 'hello' words.";
-    } else {
-      result = "Not quite there yet. Keep searching!";
-    }
-
-    document.getElementById('result').textContent = result;
-  }
-
-  return (
-    <>
-      <div>
-        <h2>Image Upload</h2>
-        <img src={imageSrc} alt="Game" />
-
-        <label htmlFor="count-input">How many 'hello' words can you find?</label>
-        <input type="number" id="count-input" value={count} onChange={handleCountChange} />
-        <button onClick={checkCount}>Check</button>
-        <p id="result"></p>
-      </div>
-    </>
-  );
 }
+
+window.addEventListener('DOMContentLoaded', () => {
+  const countInput = document.getElementById('count-input');
+  const checkBtn = document.getElementById('check-btn');
+  const image = document.createElement('img');
+  image.src = imageSrc;
+  image.alt = 'Game';
+  document.body.appendChild(image);
+
+  checkBtn.addEventListener('click', checkCount);
+});
