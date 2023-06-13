89f3191f93ddaee4fd20a1d8b73be4f3deebed56
Hannah Pham <39138220+HanhPham1512@users.noreply.github.com>
2023-06-13 15:27:47 +1000

    Update projectsData.js

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,36 +1,26 @@
 const projectsData = [
   {
     title: 'BENIT',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
+    description:'PTE Master is an AI and Machine Learning-powered platform designed to enhance students' PTE (Pearson Test of English) skills. It offers a seamless practice experience, enabling students to practice and improve their proficiency in PTE.',
     imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    href: 'https://www.masterpte.com.au/platform/#/',
   },
   {
     title: 'Mentem',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
+    description: `Mentem specializes in building learning cultures within organizations, empowering them to constantly upskill and reskill their workforce. Through our Learning Platform, we enable organizations to prepare their employees for the present and future challenges by providing a comprehensive solution for continuous learning and development.`,
     imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    href: 'https://www.mentem.co/about/whatwedo/',
   },
   
     {
-    title: 'BENIT',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
+    title: 'The Break',
+    description: `The Break is an app that utilizes web scraping to extract data directly from webpages. This approach eliminates the need for a database to store the data. Additionally, the access day is recorded as the exact time when the user connects to the webpage. This allows for real-time data retrieval and up-to-date information for users of the app.`,
     imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
+    },
   {
-    title: 'Mentem',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
+    title: 'Ola Cabs ',
+    description: `I created business rules, modeled ER diagrams, and implemented the database for the project. Additionally, I delivered project status updates according to the schedule and continuously monitored the progress, addressing any obstacles that hindered the achievement of milestones.`,
     imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
   },
 ]
 
