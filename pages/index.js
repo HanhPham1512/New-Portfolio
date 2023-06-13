585593574bd423bea855596e8e2274fd70e59513
Hannah Pham <39138220+HanhPham1512@users.noreply.github.com>
2023-06-13 14:58:31 +1000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,8 @@
+import Link from '@/components/Link'
+import { PageSEO } from '@/components/SEO'
+import Tag from '@/components/Tag'
+import siteMetadata from '@/data/siteMetadata'
+import { getAllFilesFrontMatter } from '@/lib/mdx'
+import formatDate from '@/lib/utils/formatDate'
+
 
