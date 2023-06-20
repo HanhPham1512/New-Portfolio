d553b3082c35b05b9579cafb4037445aaa1a2ca3
Hannah Pham <39138220+HanhPham1512@users.noreply.github.com>
2023-06-20 22:45:56 +1000

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,21 +1,25 @@
 import { MDXLayoutRenderer } from '@/components/MDXComponents'
 import { getFileBySlug } from '@/lib/mdx'
 
-const DEFAULT_LAYOUT = 'AuthorLayout'
+const images = [
+  '/static/images/h1.png',
+  '/static/images/h2.png',
+  '/static/images/h3.png',
+  '/static/images/h4.png',
+  // Add more image URLs here
+];
 
-export async function getStaticProps() {
-  const authorDetails = await getFileBySlug('authors', ['default'])
-  return { props: { authorDetails } }
-}
-
-export default function About({ authorDetails }) {
-  const { mdxSource, frontMatter } = authorDetails
-
+const Gallery = () => {
   return (
-    <MDXLayoutRenderer
-      layout={frontMatter.layout || DEFAULT_LAYOUT}
-      mdxSource={mdxSource}
-      frontMatter={frontMatter}
-    />
-  )
-}
+    <div className="flex flex-wrap">
+      {images.map((image, index) => (
+        <div key={index} className="w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 p-2">
+          <img src={image} alt={`Image ${index + 1}`} className="w-full h-auto" />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Gallery;
+
