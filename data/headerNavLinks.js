825ca6afd66d15ff882b05a8d0aafdf768aae463
Hannah Pham <39138220+HanhPham1512@users.noreply.github.com>
2023-06-20 22:15:53 +1000

    Update headerNavLinks.js

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,7 +1,7 @@
 const headerNavLinks = [
   { href: '/tags', title: 'Downloads' },
   { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
+  { href: '/about', title: 'Gallery' },
 ]
 
 export default headerNavLinks
