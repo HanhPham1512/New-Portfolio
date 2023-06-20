f88d297a683442b57ed6e05d0b37baa185aeafcf
Hannah Pham <39138220+HanhPham1512@users.noreply.github.com>
2023-06-20 23:17:57 +1000

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -2,27 +2,27 @@ import { MDXLayoutRenderer } from '@/components/MDXComponents'
 import { getFileBySlug } from '@/lib/mdx'
 import Image from 'next/image';
 
-const images = [
-  '/static/images/h1.png',
-  '/static/images/h2.png',
-  '/static/images/h3.png',
-  '/static/images/h4.png',
-  // Add more image URLs here
-];
-
-const Gallery = () => {
+export default function Gallery() {
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-      {images.map((image, index) => (
-        <div key={index} className="relative w-full aspect-w-1 aspect-h-1">
-          <div className="bg-white shadow-lg rounded-lg overflow-hidden">
-            <Image src={image} alt={`Image ${index + 1}`} layout="fill" objectFit="cover" />
-          </div>
-        </div>
-      ))}
+    <>
+      <PageSEO title={`Gallery - ${siteMetadata.author}`} description="My Project Gallery" />
+      <div className="mt-24 flex flex-col items-center justify-center">
+        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10">
+          My Project Gallery
+        </h1>
+  
+         <div class="flex flex-wrap">
+        <div class="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-2">
+      <img src="/static/images/h1.png" alt="Image 1" class="w-full h-auto">
     </div>
-  );
-};
-
-export default Gallery;
-
+    <div class="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-2">
+      <img src="/static/images/h2.png" alt="Image 2" class="w-full h-auto">
+    </div>
+    <div class="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-2">
+      <img src="/static/images/h3.png" alt="Image 3" class="w-full h-auto">
+    </div>
+        </div>
+ 
+    </>
+  )
+}
