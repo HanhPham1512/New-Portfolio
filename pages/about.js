892fe30afa265da5efae7c3971d0832bfeb4387c
Hannah Pham <39138220+HanhPham1512@users.noreply.github.com>
2023-06-20 23:01:45 +1000

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -14,8 +14,8 @@ const Gallery = () => {
   return (
     <div className="flex flex-wrap">
       {images.map((image, index) => (
-        <div key={index} className="p-2 w-full md:w-1/2 lg:w-1/3 xl:w-1/4">
-          <div className="relative w-full h-0 pb-2/3">
+        <div key={index} className="w-full p-2 md:w-1/2 lg:w-1/3 xl:w-1/4">
+          <div className="relative h-0 pb-2/3 w-full">
             <Image src={image} alt={`Image ${index + 1}`} layout="fill" objectFit="cover" />
           </div>
         </div>
