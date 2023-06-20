f2baf77ca53d49e1358b3cb36ded7736ce2acdfd
Hannah Pham <39138220+HanhPham1512@users.noreply.github.com>
2023-06-21 00:05:16 +1000

    Update gallery.js

--- data/gallery.js
+++ data/gallery.js
@@ -2,39 +2,20 @@ const projectsData = [
 
   {
     title: 'Haha',
-    description:'PTE Master is an AI and Machine Learning-powered platform designed to enhance students PTE (Pearson Test of English) skills. It offers a seamless practice experience, enabling students to practice and improve their proficiency in PTE.',
-    imgSrc: '/static/images/Tata_logo.svg.png',
-    href: 'https://github.com/HanhPham1512/TATA-Data-Visualization',
+    imgSrc: '/static/images/h1.png',
   },
   {
     title: 'Deloitte',
-    description: `Mentem specializes in building learning cultures within organizations, empowering them to constantly upskill and reskill their workforce. Through our Learning Platform, we enable organizations to prepare their employees for the present and future challenges by providing a comprehensive solution for continuous learning and development.`,
-    imgSrc: '/static/images/download.png',
-    href: 'https://www.mentem.co/about/whatwedo/',
+    imgSrc: '/static/images/h2.png',
   },
 
   {
     title: 'BENIT',
-    description:'PTE Master is an AI and Machine Learning-powered platform designed to enhance students PTE (Pearson Test of English) skills. It offers a seamless practice experience, enabling students to practice and improve their proficiency in PTE.',
-    imgSrc: '/static/images/PTE-master.png',
-    href: 'https://www.masterpte.com.au/platform/#/',
+    imgSrc: '/static/images/h3.png',
   },
   {
     title: 'Mentem',
-    description: `Mentem specializes in building learning cultures within organizations, empowering them to constantly upskill and reskill their workforce. Through our Learning Platform, we enable organizations to prepare their employees for the present and future challenges by providing a comprehensive solution for continuous learning and development.`,
-    imgSrc: '/static/images/MT.png',
-    href: 'https://www.mentem.co/about/whatwedo/',
-  },
-  
-    {
-    title: 'The Break',
-    description: `The Break is an app that utilizes web scraping to extract data directly from webpages. This approach eliminates the need for a database to store the data. Additionally, the access day is recorded as the exact time when the user connects to the webpage. This allows for real-time data retrieval and up-to-date information for users of the app.`,
-    imgSrc: '/static/images/theB.jpg',
-    },
-  {
-    title: 'Ola Cabs ',
-    description: `I created business rules, modeled ER diagrams, and implemented the database for the project. Additionally, I delivered project status updates according to the schedule and continuously monitored the progress, addressing any obstacles that hindered the achievement of milestones.`,
-    imgSrc: '/static/images/ola.png',
+    imgSrc: '/static/images/h4.png',
   },
 ]
 
