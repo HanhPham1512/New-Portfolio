bcd58fe1c9b4f795aea7765f830d315630399b20
Hannah Pham <39138220+HanhPham1512@users.noreply.github.com>
2023-06-20 23:20:24 +1000

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,6 @@
 import { MDXLayoutRenderer } from '@/components/MDXComponents'
 import { getFileBySlug } from '@/lib/mdx'
-import Image from 'next/image';
+
 
 export default function Gallery() {
   return (
@@ -10,19 +10,19 @@ export default function Gallery() {
         <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10">
           My Project Gallery
         </h1>
-  
-         <div class="flex flex-wrap">
-        <div class="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-2">
-      <img src="/static/images/h1.png" alt="Image 1" class="w-full h-auto">
-    </div>
-    <div class="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-2">
-      <img src="/static/images/h2.png" alt="Image 2" class="w-full h-auto">
-    </div>
-    <div class="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-2">
-      <img src="/static/images/h3.png" alt="Image 3" class="w-full h-auto">
-    </div>
+
+        <div className="flex flex-wrap">
+          <div className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-2">
+            <Image src="/static/images/h1.png" alt="Image 1" layout="responsive" width={500} height={300} />
+          </div>
+          <div className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-2">
+            <Image src="/static/images/h2.png" alt="Image 2" layout="responsive" width={500} height={300} />
+          </div>
+          <div className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-2">
+            <Image src="/static/images/h3.png" alt="Image 3" layout="responsive" width={500} height={300} />
+          </div>
         </div>
- 
+      </div>
     </>
   )
 }
